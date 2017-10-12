3fa8307934d41eb7516aae93cd8b696c7a4f9ef0
Gerson Flores Acuña <gerson71fa@hotmail.com>
2017-10-11 23:05:39 -0500

    fix url resturant images

--- api/controllers/restaurant.js
+++ api/controllers/restaurant.js
@@ -40,7 +40,7 @@ async function restaurant_update(req, res, next) {
         if (req.body.image) {
             let fileName = Date.now()
             let filepath = base64Img.imgSync(req.body.image, path.join('/home/gerson/bcode-api/public/images', 'restaurant'), fileName)
-            restaurant.image = '/images/restaurant' + fileName + path.extname(filepath)
+            restaurant.image = '/images/restaurant/' + fileName + path.extname(filepath)
         }
 
         await restaurant.save()
